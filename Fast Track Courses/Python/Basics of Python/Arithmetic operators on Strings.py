ce53d710a063455b76640d293bc0b83269ed8508
prash4931 <39942340+prash4931@users.noreply.github.com>
2021-10-01 20:52:50 +0530

    Arithmetic operators on Strings
    Print “InterviewBit” 100 times without space on a same line.

---
+++ Fast Track Courses/Python/Basics of Python/Arithmetic operators on Strings.py
@@ -0,0 +1,11 @@
+def main():
+    # YOUR CODE GOES HERE
+    # Please take input and print output to standard input/output (stdin/stdout)
+    # E.g. 'input()/raw_input()' for input & 'print' for output
+    a = "InterviewBit" * 100
+    print(a)
+
+    return 0
+
+if __name__ == '__main__':
+    main()
