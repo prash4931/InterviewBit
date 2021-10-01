89f0af6d4c52cfc6e85e731494af565317fba7d2
prash4931 <39942340+prash4931@users.noreply.github.com>
2021-10-01 20:51:24 +0530

    Arithmetic operators on Numbers
    The provided code stub reads two integers from STDIN, A and B. Add code to print seven lines where:
    
    First line shows the sum of two integers A and B.
    Second line shows the difference between two numbers(A-B).
    Third line shows the product of the two integers A and B.
    Fourth line shows the Integer Division where A is the numerator and B is the denominator.
    Fifth line shows the Float Division where A is the numerator and B is the denominator.
    Sixth line shows the Remainder when A is divided by B.
    Seventh line shows the Power i.e AB

---
+++ Fast Track Courses/Python/Basics of Python/Arithmetic operators on Numbers.py
@@ -0,0 +1,17 @@
+def main():
+    A = int(input())
+    B = int(input())
+    # Print seven lines as described above
+    #TODO
+    print(A+B)
+    print(A-B)
+    print(A*B)
+    print(A//B)
+    print(A/B)
+    print(A%B)
+    print(A ** B)
+    #TODO_END
+    return 0
+
+if __name__ == '__main__':
+    main()
