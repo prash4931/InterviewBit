f73b455cdea2005e3e0ed4193fbe83c507bf2b61
prash4931 <39942340+prash4931@users.noreply.github.com>
2021-10-01 21:01:59 +0530

    Loops And Jump Statements
    You are given an integer N, print all the odd values, for all i, where 0 <= i < N. Print each values on a seperate line.

---
+++ Fast Track Courses/Python/Flow Control & Functions/Loops And Jump Statements.py
@@ -0,0 +1,10 @@
+def main():
+    N = int(input())
+    # YOUR CODE GOES HERE
+    for i in range(1, N):
+        if(i%2!=0):
+            print(i)
+    return 0
+
+if __name__ == '__main__':
+    main()
