00ae9f71e2d96d29b51e852019b4051d91f47197
Pacific <39942340+prash4931@users.noreply.github.com>
2021-10-04 16:32:30 +0530

    StringBuffer
    You need to complete the solve() method in the below code.
    
    solve() method takes three arguments:
    
    String S
    Integer L
    Integer R
    You need to reverse the substring starting at index L and ending at index R in String S, and finally return the final string.

---
+++ Fast Track Courses/Java/Strings/StringBuffer.java
@@ -0,0 +1,27 @@
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //Don't alter anything here.
+        Scanner inp = new Scanner(System.in);
+        String S = inp.nextLine();
+        int L = inp.nextInt();
+        inp.nextLine();
+        int R = inp.nextInt();
+        inp.nextLine();
+        inp.close();
+        
+        System.out.println(solve(S,L,R));
+        /**************************************/
+    }
+    
+    //complete the function below
+    
+    public static String solve(String s, int L, int R)
+    {
+        StringBuffer sbf = new StringBuffer(s.substring(L,R+1));
+        s = s.substring(0,L)+sbf.reverse().toString()+s.substring(R+1,s.length());
+        return s;
+    }
+}
