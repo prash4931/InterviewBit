e976bbb559575e90147cff58b0b4d8420996c686
Pacific <39942340+prash4931@users.noreply.github.com>
2021-10-12 14:11:56 +0530

    Loops
    You are given an integer N, print all the odd values, for all i, where 0 <= i < N. Print each values on a seperate line.

---
+++ Fast Track Courses/Javascript/Flow Control/Loops.js
@@ -0,0 +1,7 @@
+// DO NOT USE ARGUMENTS FOR INPUTS
+// Input num valus is in variable *num*
+var num = parseInt(readLine().trim());
+// YOUR CODE GOES HERE
+for (i = 1; i < num; i=i+2) {
+    console.log(i);
+}
