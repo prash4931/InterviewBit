886587a15de375fd6f71e5463261cb4aa7bacb51
Pacific <39942340+prash4931@users.noreply.github.com>
2021-10-12 14:06:15 +0530

    Input And Output
    Take a user input string and print space separated “Hello” and input (without quotes).

---
+++ Fast Track Courses/Javascript/Js Introduction/Input And Output.js
@@ -0,0 +1,6 @@
+// Please take input and print output to standard input/output (stdin/stdout)
+// DO NOT USE ARGUMENTS FOR INPUTS
+// YOUR CODE GOES HERE
+
+var name = readLine()
+console.log("Hello "+name)
