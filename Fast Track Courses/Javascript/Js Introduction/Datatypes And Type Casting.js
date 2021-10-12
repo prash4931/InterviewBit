ec763c92954b8cd2b12e4cda02ac033ab06becb4
Pacific <39942340+prash4931@users.noreply.github.com>
2021-10-12 14:08:40 +0530

    Datatypes And Type Casting
    Given an input string which represents a positive number, you have to print the next greater integer.

---
+++ Fast Track Courses/Javascript/Js Introduction/Datatypes And Type Casting.js
@@ -0,0 +1,5 @@
+// Please take input and print output to standard input/output (stdin/stdout)
+// DO NOT USE ARGUMENTS FOR INPUTS
+// YOUR CODE GOES HERE
+var a = parseInt(readLine())
+console.log(a+1);
