1dbd205b7473d78d1e515f7e41d4d7e1300a966e
Pacific <39942340+prash4931@users.noreply.github.com>
2021-10-13 13:26:23 +0530

    Arrays In JS
    You are given array of integers with it’s size. Update and then print array elememts where new_arr[i] = old_arr[i] + i. Please note that you have to print one element in one line.

---
+++ Fast Track Courses/Javascript/Arrays And Objects/Arrays In JS.js
@@ -0,0 +1,11 @@
+// DO NOT USE ARGUMENTS FOR INPUTS
+// Vaiable size_ is the size of input array.
+// Variable array_ stores all the array elements
+var size_ = parseInt(readLine().trim());
+var array_ = []
+for(var i = 0 ; i < size_; i++ ){
+    var x = parseInt(readLine().trim());
+    array_.push(x+i);                   //Optimized solution
+    console.log(array_[i]);             //Optimized solution
+}
+// YOUR CODE GOES HERE
