eb835f3ae488754e54739d161dd177425e22ed16
Pacific <39942340+prash4931@users.noreply.github.com>
2021-10-15 12:33:59 +0530

    Classes And Their Instances
    For the given class Employee, define a constructor function so that we can initialise the name, position and skill properties with default input strings while creating instances of the class.

---
+++ Fast Track Courses/Javascript/OOPs/Classes And Their Instances.js
@@ -0,0 +1,17 @@
+// Please take input and print output to standard input/output (stdin/stdout)
+// DO NOT USE ARGUMENTS FOR INPUTS
+
+var defaultName = readLine();  
+var defaultPosition = readLine();
+var defaultSkill = readLine();
+
+class Employee{
+// define the constructor function here
+    
+    constructor(name,position,skill) {
+        this.name = defaultName;
+        this.position = defaultPosition;
+        this.skill = defaultSkill;
+    }
+
+}
