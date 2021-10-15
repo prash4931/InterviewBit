f975fa362bce54b80e6cb5bf296be995be1d8ec6
Pacific <39942340+prash4931@users.noreply.github.com>
2021-10-15 12:12:42 +0530

    Functions In JS
    You are given three numbers. You have to implement one function which will return the product of those numbers. Please note that you don’t have to print values, implement the function which takes three parameters and return the product as mentioned in the editor below.

---
+++ Fast Track Courses/Javascript/Functions And Parameters/Functions In JS.js
@@ -0,0 +1,17 @@
+// DO NOT USE ARGUMENTS FOR INPUTS
+// You have to just implement the myFunction.
+
+var a = parseInt(readLine().trim());
+var b = parseInt(readLine().trim());
+var c = parseInt(readLine().trim());
+
+// YOUR CODE GOES HERE
+
+
+/*
+var z = myFunction(a,b,c);
+console.log(z);
+*/
+function myFunction(a,b,c) {
+    return a * b * c;
+}
