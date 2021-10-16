4f6a047db0ef88e2ec47736a3e93a5061d787c7d
Pacific <39942340+prash4931@users.noreply.github.com>
2021-10-16 20:06:21 +0530

    Math Object
    You are given a positive number A denoting the radius of a sphere as input. You have to calculate and print the volume of the sphere.
    
    Volume of a sphere having radius R is given by (4 * π * R3) / 3.
    
    NOTE: Since, the answer can be a real number, you have to print the ceil value of the volume. Ceil value of a real number x is the smallest integer that is greater than or equal to x.

---
+++ Fast Track Courses/Javascript/Extras/Math Object.js
@@ -0,0 +1,5 @@
+// Please take input and print output to standard input/output (stdin/stdout)
+// DO NOT USE ARGUMENTS FOR INPUTS
+// YOUR CODE GOES HERE
+var A = parseInt(readLine().trim());
+console.log(Math.ceil((4 * Math.PI * Math.pow(A,3))/3))
