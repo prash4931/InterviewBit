45fad932a4ff4a82bb6c2dc7dfe1fe4a2cff04be
Pacific <39942340+prash4931@users.noreply.github.com>
2021-10-18 20:22:38 +0530

    Hello World!
    Write a code to print “Hello, InterviewBit!” (without quotes) in the editor below.

---
+++ Fast Track Courses/C#/Introduction/Hello World!.cs
@@ -0,0 +1,12 @@
+using System;
+using System.IO;
+
+class MAIN  {
+    public static void Main(string[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUT
+        // E.g. 'StreamReader' for input & 'StreamWriter' for output
+        Console.WriteLine("Hello, InterviewBit!");
+    }
+}
