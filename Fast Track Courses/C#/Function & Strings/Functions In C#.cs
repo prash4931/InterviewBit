4cf6fc74ecc21acc88d123f44ab9396413e88be4
Pacific <39942340+prash4931@users.noreply.github.com>
2021-10-24 23:26:57 +0530

    Functions In C#
    Create a function name addition which takes two integers as input and returns their sum as output.

---
+++ Fast Track Courses/C#/Function & Strings/Functions In C#.cs
@@ -0,0 +1,17 @@
+using System;
+using System.IO;
+
+class MAIN  {
+
+    // declare the function here
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+    
+    public static void Main(string[] args) {
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int sum = addition(a, b);
+        Console.WriteLine(sum);
+    }
+}
